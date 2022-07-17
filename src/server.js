a5bcefebc9a422b0d139541599ad9aeea3b58aab
WiseThoughts <benny.nebreda@gmail.com>
2022-07-17 01:06:22 +0100

    basic mongoose API template for team 1 final project

---
+++ src/server.js
@@ -0,0 +1,18 @@
+require("./db/connection.js")
+const express = require("express"); 
+const cors = require("cors");
+const userRouter = require("./user/routes");
+const app = express();
+const port = process.env.PORT || 5001;
+
+
+app.use(express.json());
+
+app.use(cors());
+app.use(userRouter);
+
+
+
+app.listen(port, ()=>{
+    console.log(`listening on port ${port}`);
+}); 
---
+++ src/user/routes.js
@@ -0,0 +1,10 @@
+const {Router} = require("express");
+const { } = require("./controllers");
+const {} = require ("../middleware/index.js")
+
+const userRouter = Router();
+
+
+
+
+module.exports = userRouter;
