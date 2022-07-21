56919c35b83cf51144acc81e824b65c1c3a513fa
EvoFox <foulkes.joshua@live.co.uk>
2022-07-21 15:27:12 +0100

    delete inaccessible route group routes by endpoint

--- src/user/routes.js
+++ src/user/routes.js
@@ -6,15 +6,15 @@ const {  hashPass, comparePass, tokenCheck, verifyEmail } = require ("../middlew
 const userRouter = Router();
 
 userRouter.post("/user", verifyEmail, hashPass, signUp); // define a post request on /user endpoint that calls the signUp controller
-userRouter.post("/user", hashPass, signUp); // define a post request on /user endpoint that calls the signUp controller
+userRouter.get("/user/:username", listUser);
+userRouter.get("/user", findAll);
+userRouter.patch("/user", addAddress);
 
-userRouter.post("/login", comparePass, login); // define a get request on /log-in endpoint that calls the logIn controller
+userRouter.post("/login", comparePass, login); // define a post request on /log-in endpoint that calls the logIn controller
 userRouter.get("/login", tokenCheck, login); // define a get request on /user endpoint that calls the tokenCheck and login methods
 
 userRouter.put("/change-password", comparePass, hashPass, changePassword); // define a post request on /change-password endpoint that calls the changePassword controller
-userRouter.get("/user/:username", listUser);
-userRouter.get("/user", findAll);
+
 userRouter.delete("/delete-account", tokenCheck, deleteUser); // define a delete request on /delete-account endpoint that calls the deleteUser controller
-userRouter.patch("/user", addAddress);
 
 module.exports = userRouter;
