956dfb24301c99e8795295b28b81cdb8b8996fc4
Keshia Collins <keshia90.kc@googlemail.com>
2022-07-21 14:33:08 +0100

    changed addAddress to updateUserDetails-more flexibilty

--- src/user/routes.js
+++ src/user/routes.js
@@ -1,5 +1,5 @@
 const {Router} = require("express");
-const { login, signUp, changePassword, deleteUser, listUser, findAll, addAddress } = require("./controllers");
+const { login, signUp, changePassword, deleteUser, listUser, findAll,  updateUserDetails } = require("./controllers");
 const {  hashPass, comparePass, tokenCheck, verifyEmail } = require ("../middleware/index.js")
 
 const userRouter = Router();
@@ -14,6 +14,6 @@ userRouter.put("/change-password", comparePass, hashPass, changePassword); // de
 userRouter.get("/user/:username", listUser);
 userRouter.get("/user", findAll);
 userRouter.delete("/delete-account", tokenCheck, deleteUser); // define a delete request on /delete-account endpoint that calls the deleteUser controller
-userRouter.patch("/user", addAddress);
+userRouter.patch("/user", updateUserDetails);
 
 module.exports = userRouter;
