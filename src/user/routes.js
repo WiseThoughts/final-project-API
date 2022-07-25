7a8a7fcafffb833d6ede60c304c4d768861308ab
EvoFox <foulkes.joshua@live.co.uk>
2022-07-25 12:02:59 +0100

    Added some debugging to the API

--- src/user/routes.js
+++ src/user/routes.js
@@ -1,23 +1,36 @@
-const {Router} = require("express");
-const { login, signUp, changePassword, deleteUser, listUser, findAll,  updateUserDetails, updatePassword } = require("./controllers");
-const { hashPass, comparePass, tokenCheck, verifyEmail } = require ("../middleware/index.js")
+const { Router } = require("express");
+const {
+	login,
+	signUp,
+	changePassword,
+	deleteUser,
+	listUser,
+	findAll,
+	updateUserDetails,
+	updatePassword,
+} = require("./controllers");
+const {
+	hashPass,
+	comparePass,
+	tokenCheck,
+	verifyEmail,
+} = require("../middleware/index.js");
+
+
 const userRouter = Router();
 
-
 userRouter.post("/user", verifyEmail, hashPass, signUp); // define a post request on /user endpoint that calls the signUp controller
-userRouter.post("/user", hashPass, signUp); // define a post request on /user endpoint that calls the signUp controller
+userRouter.get("/user/:username", listUser);
+userRouter.get("/user", findAll);
+userRouter.patch("/user", updateUserDetails);
 
 userRouter.post("/login", comparePass, login); // define a get request on /log-in endpoint that calls the logIn controller
 userRouter.get("/login", tokenCheck, login); // define a get request on /user endpoint that calls the tokenCheck and login methods
 
 userRouter.put("/change-password", comparePass, hashPass, changePassword); // define a post request on /change-password endpoint that calls the changePassword controller
 userRouter.put("/password", hashPass, updatePassword);
-userRouter.get("/user/:username", listUser);
-userRouter.get("/user", findAll);
+
 userRouter.delete("/delete-account", tokenCheck, deleteUser); // define a delete request on /delete-account endpoint that calls the deleteUser controller
-userRouter.patch("/user", updateUserDetails);
-
-
 
 
 module.exports = userRouter;
